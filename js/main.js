7b6774afee64246c23f9c6a0583e119925e28d2e
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-07-28 10:36:22 -0300

    carousel peliculas JS var

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelector('.pelicula');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+// Event listener para la flecha derecha //
