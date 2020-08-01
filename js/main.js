a9e1da13ebc06c47a6abb4ebe4a62fe7c5ebb3dd
santiagolopezsieiro <santiagolopezsieiro@gmail.com>
2020-08-01 10:43:10 -0300

    indicadores

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 const fila = document.querySelector('.contenedor-carousel');
-const peliculas = document.querySelector('.pelicula');
+const peliculas = document.querySelectorAll('.pelicula');
 
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
 // Event listener para la flecha derecha //
+
+flechaDerecha.addEventListener("click", () => {
+    fila.scrollLeft +=fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    
+})
+
+// Event listener para la flecha izquierda //
+
+flechaIzquierda.addEventListener("click", () => {
+    fila.scrollLeft -=fila.offsetWidth
+});
+
+// ------------------- Paginacion -------------------- //
+
+const numeroPaginas = Math.ceil(peliculas.length / 5);
+for(let i = 0; i < numeroPaginas; i++) {
+    const indicador = document.createElement('button');
+
+    if(i == 0){
+        indicador.classList.add('activo')
+    }
+    document.querySelector('.indicadores').appendChild(indicador)
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document.querySelector('.indicadores .activo').classList.remove('activo')
+        e.target.classList.add('activo');
+    })
+}
+
